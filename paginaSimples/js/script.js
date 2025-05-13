9672d1e531ca7373c0965a6fea975d75f3d2edaa
pedrocolossal <61233826+pedrocolossal@users.noreply.github.com>
2025-05-13 10:57:00 -0300

    js script pagina simples uninter

---
+++ paginaSimples/js/script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById('mensagemMotivacional');
+const mensagem = document.getElementById('mensagem');
+
+
+botao.addEventListener('click', function(){
+    mensagem.style.display = 'block';
+});
